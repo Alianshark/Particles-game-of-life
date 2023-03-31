c918bf9f7997f8e620e037e0a056699e10ef7e20
Enormouse <johnnysalvia@gmail.com>
2023-03-31 03:48:58 +0300

    delete console logs

--- app.js
+++ app.js
@@ -14,29 +14,22 @@ let context = canvas.getContext('2d');
 canvas.width = '1000';
 canvas.height = '700';
 
-console.log(context);
-console.log('Hello World');
-
 gameLoop();
 
 function gameLoop() {
     setTimeout(gameLoop, 1000/60);
-    console.log('time');
     
     context.fillStyle = '#777';
     context.fillRect(0,0,1000,700); //ochistka
     
-
     context.beginPath();
     context.arc(particle.x,particle.y,particle.r,0,2*Math.PI,false);
     context.fillStyle = 'blue';
     context.fill();
     
-
     particle.x += particle.vx;
     particle.y += particle.vy;
 
-    
     if (particle.x < 0 || particle.x > 1000) {
         particle.vx = particle.vx * (-1);
     }
