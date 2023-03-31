97cba5fae75fd3e43944c92d0e58f37731829a57
Enormouse <johnnysalvia@gmail.com>
2023-03-31 03:58:55 +0300

    create multiple function for code from gameLoop

--- app.js
+++ app.js
@@ -21,23 +21,34 @@ function clearScreen () {
     context.fillRect(0,0,1000,700); 
 }
 
-function gameLoop() {
-    setTimeout(gameLoop, 1000/60);
-    
-    clearScreen();
+function renderParticle () {
     context.beginPath();
     context.arc(particle.x,particle.y,particle.r,0,2*Math.PI,false);
     context.fillStyle = 'blue';
     context.fill();
-    
+}
+
+function moveParticle () {
     particle.x += particle.vx;
     particle.y += particle.vy;
+}
 
+function reflection () {
     if (particle.x < 0 || particle.x > 1000) {
         particle.vx = particle.vx * (-1);
     }
     if (particle.y < 0 || particle.y > 700) {
         particle.vy = particle.vy * (-1);
     }
+}
+
+function gameLoop() {
+    setTimeout(gameLoop, 1000/60);
+    
+    clearScreen();
+    renderParticle();
+    moveParticle();
+    reflection();
+
     
 }
